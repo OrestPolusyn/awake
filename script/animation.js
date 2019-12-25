26bc82f276a54aa7c1ff38766401c9e5732c88ef
OrestPolusyn <44906398+OrestPolusyn@users.noreply.github.com>
2019-12-25 11:32:05 +0200

    add pres

--- script/animation.js
+++ script/animation.js
@@ -9,15 +9,18 @@ $(".block-info").click(function () {
 });
 $(".btn-order").click(function () {
     $(".overlay").show();
-    $(".overlay").css("position","fixed")
+    $(".overlay").css("position", "fixed")
 });
+
 $(".btn-conect").click(function () {
     $(".overlay").show();
     $(".agr-img").hide();
+});
 
-})
 $(".res-img").click(function () {
-    $(".overlay").css("display","none");
+    $(".overlay").css("display", "none");
+    $(".popup").css("display", "block");
+    $(".send-result").css("display", "none");
 
 });
 
@@ -25,10 +28,10 @@ $(".btn-send").click(function () {
     $(".popup").hide();
     $(".send-result").show();
     $(".overlay").show();
-    $(".overlay").css("position","fixed");
+    $(".overlay").css("position", "fixed");
     $(".agr-img").hide();
-
-       
+    $(".confir")[0].reset();
+    $(".pop-confir")[0].reset();
 });
 
 
@@ -36,6 +39,11 @@ $(".btn-more").click(function () {
     $(".rpblock").show();
     $(".last-block").css("margin-bottom", "46px");
     $(".btn-more").hide();
+});
+
+$(".overlay-img").click(function () {
+    $(".overlay-img").hide();
+    $(".overlay-img img").css("display", "none");
 });
 
 $("a#check").click(function () {
@@ -52,12 +60,35 @@ $(".agr-img").click(function () {
     $(".res-img").show();
 });
 
+$(".columns img").click(function () {
+    $(".overlay-img").show();
+})
+
+$(".cara").click(function () {
+    $("#cara_presentation").css("display", "block");
+});
+
+$(".ohueno").click(function () {
+   $("#ohueno").css("display", "block");
+})
+
+$(".salads").click(function () {
+    $("#salads").css("display", "block");
+});
+
+$(".nirvana").click(function () {
+    $("#nirvana").css("display", "block");
+});
+
+$(".sushi").click(function () {
+    $("#sushi").css("display", "block");
+});
 
 
 function clickCheck() {
-    const  checkBox = document.querySelectorAll("#check");
+    const checkBox = document.querySelectorAll("#check");
     const send = document.querySelectorAll(".btn-send");
-    if (checkBox[0].checked == true ){
+    if (checkBox[0].checked == true) {
         send[0].disabled = false;
     } else {
         send[0].disabled = true;
@@ -65,9 +96,9 @@ function clickCheck() {
 }
 
 function clickCheckBtm() {
-    const  checkBox = document.querySelectorAll("#check");
+    const checkBox = document.querySelectorAll("#check");
     const send = document.querySelectorAll(".btn-send");
-    if (checkBox[2].checked == true ){
+    if (checkBox[2].checked == true) {
         send[1].disabled = false;
     } else {
         send[1].disabled = true;
--- script/sendForm.js
+++ script/sendForm.js
@@ -4,17 +4,16 @@
 $(document).ready(function () {
 
 	$(".btn-style").on("click", function(e){
-		
 		$.ajax({
 			type: "POST",
-			url: 'opect.pol@i.ua',
+			url: 'awake.studio@yandex.com',
 			data: {
 				fn: $('input[name=fn]').val(),
 				tel: $('input[name=tel]').val(),
 				ms: $('textarea[name=ms]').val(),
 			},
 			success: function (result) {
-				alert('Your message send!');
+				// alert('Your message send!');
 			},
 			error: function (xhr, status, p3) {
 				alert(xhr.responseText);
@@ -26,53 +25,3 @@ $(document).ready(function () {
 		
 	
 })
-
-
-// const urlDB = "https://newsdirect.newsquantified.com/support/SendEmailFromLandingPage";
-
-// const addUser = (fn, co, em, ph, ct, ms) => {
-//     const newPost = {
-//         fn: fn,
-//         co: co,
-//         em: em,
-//         ph: ph,
-//         ct: ct,
-//         ms: ms
-//     };
-//     return fetch(urlDB, {
-//         method: 'POST',
-//         body: JSON.stringify(newPost),
-//         headers: {
-//             "Content-type": "application/json"
-//         }
-//     })
-//         .then(response => {
-//             if (response.ok) return alert('Your message send!');
-//             throw new Error('Error:' + response.statusText);
-//         })
-//         .catch(error => console.log(error));
-// }
-
-// let form = document.querySelector('.js-form'),
-//     userName = document.querySelector('input[name=fn]'),
-//     userCorporation = document.querySelector('input[name=co]'),
-//     userEmail = document.querySelector('input[name=em]'),
-//     userPhone = document.querySelector('input[name=ph]'),
-//     userCountry = document.querySelector('select[name=ct]'),
-//     userMessage = document.querySelector('textarea[name=ms]');
-
-// form.addEventListener("click", getUsersData);
-
-// function getUsersData(event) {
-//     event.preventDefault();
-//     const target = event.target;
-//     if (target.dataset.action === "create")  {
-//         addUser(userName.value,
-//             userCorporation.value,
-//             userEmail.value,
-//             userPhone.value,
-//             userCountry.value,
-//             userMessage.value)
-//             .then(alert('Your message send!'));
-//     }
-// }
